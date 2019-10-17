8195018078afe094748d960921fc9fa2738444cc
Govinda <govi2010@gmail.com>
2019-10-17 17:27:37 +0530

    get set flatten accounts

--- src/app/store/general.reducer.ts
+++ src/app/store/general.reducer.ts
@@ -3,9 +3,15 @@ import {AccountActionsUnion, AccountActionType} from '../actions/account/account
 
 export interface GeneralState {
     flattenAccounts: IFlattenAccountsResultItem[];
+    salesAccounts: IFlattenAccountsResultItem[];
+    purchaseAccounts: IFlattenAccountsResultItem[];
+    withdrawalAccounts: IFlattenAccountsResultItem[];
 }
 
 const initialState: GeneralState = {
+    purchaseAccounts: [],
+    salesAccounts: [],
+    withdrawalAccounts: [],
     flattenAccounts: []
 };
 
@@ -15,18 +21,44 @@ export function GeneralReducer(state: GeneralState = initialState, action: Accou
             return state;
 
         case AccountActionType.GetFlattenAccountsComplete:
+            const result = action.result.results;
             return {
                 ...state,
-                flattenAccounts: action.result.results
+                flattenAccounts: action.result.results,
+                salesAccounts: filterAccounts(result, 'sales'),
+                purchaseAccounts: filterAccounts(result, 'purchase'),
+                withdrawalAccounts: filterAccounts(result, 'withdrawal')
             };
 
         case AccountActionType.GetFlattenAccountsError:
             return {
                 ...state,
-                flattenAccounts: []
+                flattenAccounts: [],
+                purchaseAccounts: [],
+                salesAccounts: [],
+                withdrawalAccounts: []
             };
 
         default:
             return state;
     }
 }
+
+const filterAccounts = (accounts: IFlattenAccountsResultItem[], type: string) => {
+    switch (type) {
+        case 'sales':
+            return accounts.filter(acc => {
+                return acc.parentGroups.some(s => s.uniqueName === 'otherincome' || s.uniqueName === 'revenuefromoperations');
+            });
+
+        case 'purchase':
+            return accounts.filter(acc => {
+                return acc.parentGroups.some(s => s.uniqueName === 'operatingcost' || s.uniqueName === 'indirectexpenses');
+            });
+
+        case 'withdrawal':
+            return accounts.filter(acc => {
+                return acc.parentGroups.some(s => s.uniqueName === 'bankaccounts' || s.uniqueName === 'cash');
+            });
+    }
+};
