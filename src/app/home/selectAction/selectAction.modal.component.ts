f516564a8c41bae6c5c5dd43ccfe705c8ea28cf5
Govinda <govi2010@gmail.com>
2019-10-17 12:53:03 +0530

    basic setup done

--- src/app/home/selectAction/selectAction.modal.component.ts
+++ src/app/home/selectAction/selectAction.modal.component.ts
@@ -7,6 +7,36 @@ import {Component, OnInit} from '@angular/core';
 })
 
 export class SelectActionModalComponent implements OnInit {
+    public actions: Array<{ icon: string, style: any, color: string, name: string }> = [
+        {
+            icon: 'assets/icon/sales.svg',
+            style: {
+                border: '2px solid #45A7E6',
+                'border-bottom-width': '5px'
+            },
+            color: 'sales',
+            name: 'Sales/Income'
+        },
+        {
+            icon: 'assets/icon/purchase.svg',
+            style: {
+                border: '1px solid #F2913B',
+                'border-bottom-width': '5px'
+            },
+            color: 'purchase',
+            name: 'Purchase/Expenses'
+        },
+        {
+            icon: 'assets/icon/withdrawal.svg',
+            style: {
+                border: '1px solid #67AA5A',
+                'border-bottom-width': '5px'
+            },
+            color: 'withdrawal',
+            name: 'Withdrawal/Deposit'
+        },
+    ];
+
     constructor() {
     }
 
